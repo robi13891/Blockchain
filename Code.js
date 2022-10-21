1e79364ca7a069a57ebbd7e5f31832b19dfb774d
Roberta Beretta <robi13891@gmail.com>
2022-10-21 10:09:40 +0200

    resolved import/export error adding type in package.json

--- Code.js
+++ Code.js
@@ -2,3 +2,6 @@ import Blockchain from "./Classes/Blockchain.mjs";
 import Block from "./Classes/Block.mjs";
 import Trasaction from "./Classes/Transaction.mjs";
 
+let ADPee = new Blockchain();
+console.log(ADPee);
+
